16a81122b9a0718e0cd9deca39037ec572992492
selcip <eduselcip@gmail.com>
2022-04-27 20:18:41 -0300

    adicionando arquivos de build

---
+++ dist/esm/index.d.ts
@@ -0,0 +1,4 @@
+import type { CapacitorQRScannerPlugin } from './definitions';
+declare const CapacitorQRScanner: CapacitorQRScannerPlugin;
+export * from './definitions';
+export { CapacitorQRScanner };
